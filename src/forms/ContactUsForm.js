d33ade6b38d2b210de141dfc2d84317a71628593
AMR <annmarie@bkonnected.org>
2019-03-18 08:46:41 -0600

    finally figuring this out, contact us form looking better

--- src/forms/ContactUsForm.js
+++ src/forms/ContactUsForm.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, {Component} from 'react'
 import {Field, reduxForm} from 'redux-form'
 import asyncValidate from '../AsyncValidate'
 import {renderTextField} from "./ReduxFormUtils";
@@ -6,8 +6,8 @@ import {withStyles} from '@material-ui/core/styles';
 import {Paper} from "material-ui";
 import Typography from "@material-ui/core/Typography";
 import normalizePhone, {required} from "./ReduxFormValidator";
-import Grid from "@material-ui/core/Grid";
 import Button from "@material-ui/core/Button";
+import * as PropTypes from "prop-types";
 
 const styles = theme => ({
     root: {
@@ -19,6 +19,10 @@ const styles = theme => ({
     },
     control: {
         padding: theme.spacing.unit * 2,
+    },
+    button: {
+        marginTop: theme.spacing.unit * 3,
+        marginLeft: theme.spacing.unit,
     },
 });
 
@@ -36,63 +40,62 @@ const validate = values => {
     return errors
 }
 
-const ContactUsForm = props => {
-    const {handleSubmit, pristine, reset, submitting, classes} = props
-    return (
-        <form onSubmit={handleSubmit}>
-            <React.Fragment>
-                <div>
-                    <Paper>
-                        <Typography variant="h5" component="h3">
-                            Housing Connection Needed
-                        </Typography>
-                        <Typography component="p">
-                            B-Konnected focuses on streamlining services, housing placement and housing
-                            navigation. We attempt to connect you with landlords that will meet with your
-                            specific housing needs. The form below asks specific questions that will attempt to
-                            match you with units in our system.
-                        </Typography>
-                        <Typography component="p">
-                            All entries are time stamped and will be addressed accordingly. If you need
-                            assistance in filling out this form please e-mail bkonnectedteam@bkonnected.org
-                        </Typography>
-                        <Typography component="p">
-                            Please note: B-Konnected is not responsible for any landlord rental screenings or
-                            their application processes. B-Konnected is an equal housing provider and HUD
-                            affiliate partner. Property of B-Konnected, LLC.
-                        </Typography>
-                    </Paper>
-                </div>
-                <Grid container className={classes.root} spacing={24}>
-                    <Grid item>
-                        <Grid container spacing={8}>
-                            <Field name="firstAndLastName" component={renderTextField} label="First and Last Name*"
-                                   validate={[required]}/>
+class ContactUsForm extends Component {
+    render() {
+        const {handleSubmit, pristine, reset, submitting, classes} = this.props
+        return (
+            <form onSubmit={handleSubmit}>
+                <React.Fragment>
+                    <div>
+                        <Paper>
+                            <Typography variant="h5" component="h3">
+                                Housing Connection Needed
+                            </Typography>
+                            <Typography component="p">
+                                B-Konnected focuses on streamlining services, housing placement and housing
+                                navigation. We attempt to connect you with landlords that will meet with your
+                                specific housing needs. The form below asks specific questions that will attempt to
+                                match you with units in our system.
+                            </Typography>
+                            <Typography component="p">
+                                All entries are time stamped and will be addressed accordingly. If you need
+                                assistance in filling out this form please e-mail bkonnectedteam@bkonnected.org
+                            </Typography>
+                            <Typography component="p">
+                                Please note: B-Konnected is not responsible for any landlord rental screenings or
+                                their application processes. B-Konnected is an equal housing provider and HUD
+                                affiliate partner. Property of B-Konnected, LLC.
+                            </Typography>
+                        </Paper>
+                    </div>
+                    <div>
+                        <Field name="firstAndLastName" component={renderTextField} label="First and Last Name*"
+                               validate={[required]}/>
 
-                            <Field name="phone" component={renderTextField} label="Phone Number*"
-                                   normalize={normalizePhone} validate={[required]}/>
+                        <Field name="phone" component={renderTextField} label="Phone Number*"
+                               normalize={normalizePhone} validate={[required]}/>
 
-                            <Field name="email" component={renderTextField} label="Email*" validate={[required]}/>
-                        </Grid>
-                    </Grid>
-                    <Grid item xs={8}>
-                        <Grid container justify={"left"} spacing={8}>
-                            <Grid item>
-                                <Button type="submit" variant="contained" color="primary"
-                                        disabled={pristine || submitting}>Submit</Button>
-                            </Grid>
-                            <Grid item>
-                                <Button type="button" variant="contained" color="primary"
-                                        disabled={pristine || submitting} onClick={reset}>Clear Values</Button>
-                            </Grid>
-                        </Grid>
-                    </Grid>
-                </Grid>
+                        <Field name="email" component={renderTextField} label="Email*" validate={[required]}/>
+                    </div>
+                    <div className={classes.buttons}>
+                        <Button type="submit" variant="contained" className={classes.button}
+                                disabled={pristine || submitting}>Submit</Button>
+                        <Button type="button" variant="contained" color="primary" className={classes.button}
+                                disabled={pristine || submitting} onClick={reset}>Clear Values</Button>
+                    </div>
 
-            </React.Fragment>
-        </form>
-    )
-}
+                </React.Fragment>
+            </form>
+        )
+    }
+};
+
+ContactUsForm.propTypes = {
+    // handleSubmit: PropTypes.func.isRequired,
+    // handleBack: PropTypes.func.isRequired,
+    classes: PropTypes.object.isRequired,
+};
+
 
 export default reduxForm({
     form: 'ContactUsForm',  // a unique identifier for this form
